9860b36754bf876db46115b159252cd2de31a3c2
MrtShrzdnv <mrt.shrzdnv@gmail.com>
2017-08-14 20:18:31 +0400

    add SecondActivity

---
+++ app/src/main/java/shiriyazdanov_marat/openweathermapapp/SecondActivity.java
@@ -0,0 +1,63 @@
+package shiriyazdanov_marat.openweathermapapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import shiriyazdanov_marat.openweathermapapp.api.WeatherApi;
+import shiriyazdanov_marat.openweathermapapp.entity.CurrentWeatherModel;
+
+public class SecondActivity extends Activity {
+    private EditText editText;
+    private String strCityName;
+    protected static List<CurrentWeatherModel> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        editText = (EditText) findViewById(R.id.et);
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if((keyEvent.getAction() == KeyEvent.ACTION_DOWN) && (i == KeyEvent.KEYCODE_ENTER)
+                        && (!editText.getText().toString().equals(""))) {
+                    strCityName = editText.getText().toString();
+
+                    App.getWeatherApi().getData(strCityName,WeatherApi.WEATHER_UNITS, WeatherApi.KEY).enqueue(new Callback<CurrentWeatherModel>(){
+
+                        @Override
+                        public void onResponse(Call<CurrentWeatherModel> call, Response<CurrentWeatherModel> response) {
+                            if (response.code() == 200){
+                                CurrentWeatherModel result = response.body();
+                                SecondActivity.list.add(result);
+                                editText.setText("");
+                                Toast.makeText(SecondActivity.this,"ok",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<CurrentWeatherModel> call, Throwable t) {
+                            Log.d("TAG", "FAIL");
+                            t.getMessage();
+                        }
+                    });
+
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+}
